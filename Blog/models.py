588c9f007408cc2d94a9f3d27793f4dbf6551c5c
El-Rayyek <mohamed.abdelfattah439@eng.s-mu.edu.eg>
2022-08-17 20:24:47 +0200

    working Blog

---
+++ Blog/models.py
@@ -0,0 +1,8 @@
+from turtle import title
+from django.db import models
+
+# Create your models here.
+class post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField(max_length=10000)
+    
