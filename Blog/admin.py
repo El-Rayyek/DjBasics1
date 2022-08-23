ee2182b00c1fdc1b5476740abfd8fe5ed8761d16
El-Rayyek <mohamed.abdelfattah439@eng.s-mu.edu.eg>
2022-08-23 15:53:27 +0200

    add Blog module

--- Blog/admin.py
+++ Blog/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import post
+from .models import post , Category
 
-admin.site.register(post)
+class PostAdmin(admin.ModelAdmin):
+    list_display= ['id','title','category']
+    search_fields= ['title','content']
+    date_hierarchy= ['publish_date']
+
+
+admin.site.register(post,PostAdmin)
+admin.site.register(Category)
--- Blog/models.py
+++ Blog/models.py
@@ -1,8 +1,33 @@
+from distutils.command.upload import upload
+from tabnanny import verbose
 from turtle import title
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self) :
+        return self.name
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
 class post(models.Model):
     title = models.CharField(max_length=100)
-    content = models.TextField(max_length=10000)
-    
+    content = models.TextField(max_length=10000,null=True,blank=True)
+    publish_date = models.DateField(default= timezone.now)
+    uodate_date = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to = 'posts/')
+    category = models.ForeignKey(Category ,on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'My Post'
+        verbose_name_plural = 'Posts'
+
+
+    def __str__(self):
+        return self.title
